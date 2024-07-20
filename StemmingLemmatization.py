3ce01b6a2d2e7acc98faab28ee3f1148cbfd39bb
KoteGotiashvili <kote.gotiashvili@gmail.com>
2024-07-20 20:00:45 +0400

    add custom lemmatization of words

--- StemmingLemmatization.py
+++ StemmingLemmatization.py
@@ -23,13 +23,44 @@ words ="Running Bigger Wolves Sang Children Better Cacti Mice Went Happier"
 
 print()
 nlp = spacy.load("en_core_web_sm")
+doc = nlp(words)
+
 
 def lemmatization(words):
-    nlp = spacy.load("en_core_web_sm")
-    doc = nlp(words)
     for token in doc:
         print(f"{token}: {token.lemma_}")
 
 lemmatization(words) # works way better
+
+
+# Make custom word for lemmatization
+print()
+
+def custom_word(slang, string):
+
+    """
+    pass one slang get corrected lemma
+    :param slang: one slang word: str
+    :param string: text to be lemmatized: str
+    :return: None, void
+    """
+    # Get the attribute ruler
+    ar = nlp.get_pipe('attribute_ruler')
+
+    # Add custom rule
+    ar.add([[{"TEXT":f"{slang}"}]], {"LEMMA": "Friend"})
+
+    # Process the document
+    doc = nlp(string)
+
+    # Iterate over tokens and print their text and lemma
+    for token in doc:
+        print(token.text, " | ", token.lemma_)
+
+
+custom_word("homie","Hey my homie, how are you doing today")
+
+
+
 
 
