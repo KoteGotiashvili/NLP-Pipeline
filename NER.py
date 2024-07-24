8234493c68f6aa7eef1acb1180ed6c86755e7009
KoteGotiashvili <kote.gotiashvili@gmail.com>
2024-07-24 17:33:48 +0400

    add get_ents method, which returns ents in text

---
+++ NER.py
@@ -0,0 +1,12 @@
+import spacy
+
+nlp = spacy.load('en_core_web_sm')
+
+def get_ents(text):
+    doc = nlp(text)
+    for ent in doc.ents:
+        print(ent.text, " |", ent.label_," |", spacy.explain(ent.label_))
+
+# get ents from text
+#get_ents("Ilya Sutskever, co-founder of OpenAI, has published over 50 research papers and holds multiple AI patents.")
+
