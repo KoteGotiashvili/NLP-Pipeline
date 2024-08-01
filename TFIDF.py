b5ecf2dcbbfa158656d73899011d80a5b01369c1
KoteGotiashvili <kote.gotiashvili@gmail.com>
2024-08-01 20:22:31 +0400

    normalize data

--- TFIDF.py
+++ TFIDF.py
@@ -1,5 +1,5 @@
 from sklearn.feature_extraction.text import TfidfVectorizer
-
+import pandas as pd
 sentences = [
     "Ilya Sutskever is a leading figure in deep learning and co-founder of OpenAI.",
     "OpenAI developed the GPT-3 model, which has revolutionized natural language processing.",
@@ -27,3 +27,34 @@ sentences = [
 #     idf_score = v.idf_[indx]
 #
 #     # print(f"{word} : {idf_score}")
+
+
+## lets work on real dataset using TF IDF
+
+df = pd.read_csv('./data/emotions.csv')
+# sadness (0), joy (1), love (2), anger (3), fear (4), and surprise (5).
+#print(df.label.value_counts())
+def normalize_labels(df, target_count=14500):
+    # Create an empty DataFrame to store the normalized data
+    normalized_df = pd.DataFrame()
+
+    # Iterate through each unique label
+    for label in df['label'].unique():
+        # Filter the rows with the current label
+        label_df = df[df['label'] == label]
+
+        # Sample the rows to reach the target count
+        if len(label_df) > target_count:
+            # Randomly sample the rows if the label count is greater than the target count
+            sampled_df = label_df.sample(n=target_count, random_state=1)
+        else:
+            # If the count is less than the target count, use all rows
+            sampled_df = label_df
+
+        # Append the sampled data to the normalized DataFrame
+        normalized_df = pd.concat([normalized_df, sampled_df], ignore_index=True)
+
+    return normalized_df
+
+df = normalize_labels(df, target_count=14500)
+print(df.head())
