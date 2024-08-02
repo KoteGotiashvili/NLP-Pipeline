9e221178adf986e884eecc74cb3dded68d53f939
KoteGotiashvili <kote.gotiashvili@gmail.com>
2024-08-02 22:50:04 +0400

    keep only random forest

--- TFIDF.py
+++ TFIDF.py
@@ -1,11 +1,9 @@
 from sklearn.feature_extraction.text import TfidfVectorizer
 import pandas as pd
 from sklearn.model_selection import train_test_split
-from sklearn.neighbors import KNeighborsClassifier
 from sklearn.pipeline import Pipeline
 from sklearn.metrics import classification_report
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.naive_bayes import MultinomialNB
 
 sentences = [
     "Ilya Sutskever is a leading figure in deep learning and co-founder of OpenAI.",
