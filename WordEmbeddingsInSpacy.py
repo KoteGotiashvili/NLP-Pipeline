364335f56ed7abb7d3925b34d9ac4db71adc4257
KoteGotiashvili <kote.gotiashvili@gmail.com>
2024-08-05 14:12:16 +0400

    test on small dataset 100 sample each, turn data into vectors, accuracy is low due to data, can be better using KNN

--- WordEmbeddingsInSpacy.py
+++ WordEmbeddingsInSpacy.py
@@ -2,6 +2,11 @@ import spacy
 import numpy as np
 import pandas as pd
 from sklearn.preprocessing import LabelEncoder
+from sklearn.model_selection import train_test_split
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.metrics import classification_report
+
 
 
 nlp = spacy.load("en_core_web_lg")
@@ -32,8 +37,42 @@ df = df.drop(columns=['authors', 'date', 'link', "short_description"])
 label_encoder = LabelEncoder()
 
 df['label'] = label_encoder.fit_transform(df['category'])
+# print(df['category'].value_counts())
 
-df = df.drop(columns=['category'])
+# Determine the number of samples for each category
+min_samples = 100
+
+# Sample from each category to get exactly min_samples
+df_balanced = df.groupby('category').apply(lambda x: x.sample(min(len(x), min_samples))).reset_index(drop=True)
+
+
+df_balanced = df_balanced.drop(columns=['category'])
+
+df_balanced['vector'] = df_balanced['headline'].apply(lambda x: nlp(x).vector)
+
+
+X_train, X_test, y_train, y_test = train_test_split(
+    df_balanced.vector.values,
+    df_balanced.label,
+    test_size=0.2,
+    random_state=2022
+)
+X_train_2d = np.stack(X_train)
+X_test_2d = np.stack(X_test)
+
+
+
+scaler = MinMaxScaler()
+scaled_train_embed = scaler.fit_transform(X_train_2d)
+scaled_test_embed = scaler.transform(X_test_2d)
+
+
+clf = MultinomialNB()
+clf.fit(scaled_train_embed, y_train)
+y_pred = clf.predict(scaled_test_embed)
+
+print(classification_report(y_test, y_pred))
+
 
 
 
