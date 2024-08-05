9da23ee985605ac79c1c3361bc7f75918d357646
KoteGotiashvili <kote.gotiashvili@gmail.com>
2024-08-05 10:29:33 +0400

    get vector, shape, and comment code

---
+++ WordEmbeddingsInSpacy.py
@@ -0,0 +1,12 @@
+import spacy
+
+nlp = spacy.load("en_core_web_lg")
+
+#doc = nlp("workout running push-ups sleeping playing gamer AI ML ")
+
+#for token in doc:
+#    print(token.text, "Vector: ", token.has_vector, "OOV: ", token.is_oov)
+
+#print("print vector and shape")
+#print(doc[0].vector, doc[0].vector.shape)
+
