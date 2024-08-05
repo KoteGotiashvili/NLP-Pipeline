2fccad0bdfc7af7d3dbf1cc9d0de1648b78f1c3d
KoteGotiashvili <kote.gotiashvili@gmail.com>
2024-08-05 10:36:35 +0400

    create methoh which checks similarity betwwen base word and doc tokens

--- WordEmbeddingsInSpacy.py
+++ WordEmbeddingsInSpacy.py
@@ -10,3 +10,14 @@ nlp = spacy.load("en_core_web_lg")
 #print("print vector and shape")
 #print(doc[0].vector, doc[0].vector.shape)
 
+def check_similarity(doc, base_token):
+    for token in doc:
+        print(f"{token.text} | {base_token.text}: Get similarity: ", token.similarity(base_token))\
+
+base_token = nlp("program")
+
+doc = nlp("workout running pushups sleep playing gamer AI ML food ")
+
+check_similarity(doc, base_token)
+
+
